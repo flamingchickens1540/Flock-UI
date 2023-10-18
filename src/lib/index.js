e3fedb9be190b188b1f7f63c9f892ea6483e755f
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2023-10-17 19:07:13 -0700

    License

--- src/lib/index.js
+++ src/lib/index.js
@@ -9,3 +9,5 @@ export * from "./template/multipleChoice/MultipleChoices.svelte";
 export * from "./ui/setting/ScoreCounter.svelte";
 export * from "./ui/setting/Star.svelte";
 export * from "./ui/setting/Switch.svelte";
+export * from "./ui/match-scout/DefenseButton.svelte";
+export * from "./ui/match/RobotList.svelte";
