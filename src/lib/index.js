38968f4f1641b6c091cca4abcc78e1d2aef2bdcc
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2023-11-08 13:53:02 -0800

    updated imports

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,11 +3,13 @@ import Stackable from "./ui/structure/Stackable.svelte";
 import ScoreCounter from "./ui/setting/ScoreCounter.svelte";
 import DefenseButton from "./ui/match-scout/DefenseButton.svelte";
 import Options from "./ui/structure/Options.svelte";
+import Match from "./ui/match/Match.svelte";
 
 export {
     CatlystCarousel,
     Stackable,
     ScoreCounter,
     DefenseButton,
-    Options
+    Options,
+    Match
 };
